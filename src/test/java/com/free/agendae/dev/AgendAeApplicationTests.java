4d513daffc5e403a597114d2c0728272abc2fc95
Flávio Dias <flavio.dias.maia1@gmail.com>
2020-04-18 19:21:03 -0300

    innitial commit

---
+++ src/test/java/com/free/agendae/dev/AgendAeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free.agendae.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendAeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
